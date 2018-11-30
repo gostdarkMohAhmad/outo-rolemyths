162e7ad301b0785c250cd3643a1eef3b73c56080
gostdarkMohAhmad <44299639+gostdarkMohAhmad@users.noreply.github.com>
2018-11-30 09:51:53 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('guildMemberAdd', member=> {
+    member.addRole(member.guild.roles.find("MYTHS Cominty"));
+    });
+client.login(process.env.BOT_TOKEN);
