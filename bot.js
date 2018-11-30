9873d9186972de3edc79ddebbb90e7072ab5ae23
gostdarkMohAhmad <44299639+gostdarkMohAhmad@users.noreply.github.com>
2018-11-30 09:58:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('guildMemberAdd', member=> {
-    member.addRole(member.guild.roles.find("MYTHS Cominty"));
+    member.addRole(member.guild.roles.find("MYTHś  Community"));
     });
 client.login(process.env.BOT_TOKEN);
